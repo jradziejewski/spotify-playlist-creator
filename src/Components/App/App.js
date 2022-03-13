9cc89928c5360e3fe9af89077ba530bab695127b
jradziejewski <261309@student.pwr.edu.pl>
2022-03-13 13:22:02 +0100

    feat: test feat
    feat: sec feat

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Quick<span className="highlight">Spoti hej</span>Playlist
+                    {/* test comment */}
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
