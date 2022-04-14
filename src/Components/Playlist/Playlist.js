854ad20f507b2902b9f9308af51776793600a40d
jradziejewski <261309@student.pwr.edu.pl>
2022-04-14 16:25:59 +0200

    feat: add Playlist component HTML and CSS

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Playlist.scss';
 
 export class Playlist extends React.Component {
     render() {
-        return <div>Playlist</div>;
+        return (
+            <div className="Playlist">
+                <input value="New Playlist" />
+                {/* <!-- Add a TrackList component --> */}
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        );
     }
 }
